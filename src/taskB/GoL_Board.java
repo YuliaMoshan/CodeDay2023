8427fead08d934ae4257396c9ebde00c9b4b7a12
Vaturi <omri1991@gmail.com>
2023-05-24 15:27:01 +0300

    update

--- src/taskB/GoL_Board.java
+++ src/taskB/GoL_Board.java
@@ -1,53 +1,62 @@
 package taskB;
 
-import java.util.Iterator;
+import java.io.*;
+import java.util.*;
 
-/**
- * 
- * @author 
- * This class implements a Game of life board
- * 
- */
-public class GoL_Board extends CA
-{
-	/**
-	 * 
-	 * @param rows: Number of rows
-	 * @param cols: Number of columns
-	 * Init new GoL automaton with an empty board
-	 */
-	public GoL_Board(int rows, int cols)
-	{
-		// TODO Auto-generated constructor stub
+public class GoL_Board extends CA {
+	private String[] cell_state = {" ", "*"};
+	private int rows;
+	private int cols;
+	private int[][] board;
+	private final FileWriter board_buff = new FileWriter("board.txt");
+
+	public GoL_Board(int rows, int cols) throws IOException {
+		board = new int[rows][cols];
+		SetBoard(board);
+
 	}
 
 	@Override
-	public Iterator iterator()
-	{
+	public Iterator iterator() {
 		// TODO Auto-generated method stub
 		return null;
-	}
-	
-	/**
-	 * 
-	 * @param board: The board to be set.
-	 * Set parameter board as current board
-	 */
-	public void SetBoard(int[][] board)
-	{
-		// TODO Auto-generated method stub
 	}
 
-	/**
-	 * 
-	 * @return current CA state as a String 
-	 */
-	public String CurrentBoardOutput()
-	{
+	public void SetBoard(int[][] board) {
+		Random random = new Random();
+		for (int i = 0; i < this.rows; i++) {
+			for (int j = 0; j < this.cols; j++) {
+				int rand_state = random.nextInt(2);
+				board[i][j] = rand_state;
+			}
+		}
+	}
+
+	public void toFile(int[][] board) throws IOException {
+		try (BufferedWriter wr = new BufferedWriter(board_buff)) {
+			wr.write(board.length + " " + board[0].length);
+			wr.newLine();
+			for (int i = 0; i < board[i].length; i++) {
+				for (int j = 0; j < this.cols; j++) {
+					wr.write(board[i][j]);
+				}
+				wr.newLine();
+			}
+		}
+	}
+
+	public String CurrentBoardOutput() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	// TODO add methods and fields as needed.
-	
+
+	public void printBoard(int[][] board) {
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				System.out.print(board[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
 }
